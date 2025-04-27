8f25183eda3c12e71852b379f6458fd519041deb
TKHatton <Ltkenney13@gmail.com>
2025-04-27 11:27:17 -0400

    Back to initial commit

--- _app.js
+++
@@ -1,5 +0,0 @@
-// This file is needed for Next.js pages directory structure
-// It's a minimal version to satisfy the build requirements
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
